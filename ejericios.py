7e0bf2eb0d158bf464acda36f7f3966e275c7b81
Julian Paquez <julianpaquez@gmail.com>
2023-08-14 09:26:13 -0300

    cambioe ejercicio 1

--- ejericios.py
+++ ejericios.py
@@ -10,3 +10,6 @@ print(mensaje)
 Use una variable para representar el nombre de una persona e imprima el mensaje "Hola
 <nombre de la persona>,¿te gustaría aprender algo de Python hoy?".
 '''
+
+nombre = "Julian"
+print(f"Hola {nombre} como andas")
